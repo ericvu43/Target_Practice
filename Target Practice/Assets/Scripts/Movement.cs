2b9f52e9e262ee3cbe99aaccbdbfe5ed5e98708b
ericvu43 <ericvu43@gmail.com>
2023-10-26 01:07:02 -0400

    test

--- Target Practice/Assets/Scripts/Movement.cs
+++ Target Practice/Assets/Scripts/Movement.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    Animator animator;
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButton(0))
+        {
+            animator.SetBool("aim", true);
+            
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            animator.SetBool("aim", false);
+            animator.SetBool("shoot", true);
+        }
+        else
+        {
+            animator.SetBool("shoot", false);
+        }
     }
 }
